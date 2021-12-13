1ddc184d7a6a577c796a06d05159e15ee663353d
DKendle <dina.kendle@gmail.com>
2021-12-13 17:01:52 -0500

    Adds styling to NavLinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
+
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "14px",
+    margin: "0 6px 6px",
+    background: "purple",
+    textDecoration: "none",
+    color: "white",
+  };
+
 export default function Navbar() {
     return (
-        <div>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/activities'>Activities</NavLink>
+        <div id='navbar'>
+            <NavLink to='/' style={linkStyles}>Home</NavLink>
+            <NavLink to='/activities' style={linkStyles}>Activities</NavLink>
         </div>
     )
 }
