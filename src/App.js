729a6b62c08a092dfb309a0cbd006cd2c9d2671b
DKendle <dina.kendle@gmail.com>
2021-12-12 19:12:37 -0500

    Adds home component. Adds react-router-dom. Creates route for home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <Routes>
+          <Route path="/" element={<Home/ >}>Home</Route>
+        </Routes>
     </div>
   );
 }
