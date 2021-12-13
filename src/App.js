355e48c5882c844809bf585bd15feb7dddb3acf4
DKendle <dina.kendle@gmail.com>
2021-12-12 19:21:26 -0500

    Creates navbar component and link to activities

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header';
 import Home from './components/Home';
 import ActivityDisplay from './components/ActivityDisplay';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Header />
-    
+      <Navbar />
         <Routes>
           <Route path="/activities" element={<ActivityDisplay />} />
           <Route path="/" element={<Home/ >} />
