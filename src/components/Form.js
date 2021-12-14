1afd9571b068decaac231ddc2e44548bc6c14800
DKendle <dina.kendle@gmail.com>
2021-12-14 15:01:39 -0500

    adds handle change function to form to get form value data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
+
+//State will be used to collect changes in the form and
+//update the original state
 export default function Form() {
+
+    const [title, setTitle] = useState(0)
 
     function handleSubmit(e){
         e.preventDefault()
         console.log(e.target)
     }
 
+    function handleChange(e){
+        console.log(e.target.value)
+    }
 
     return (
         <div>
             <h1>Add a New Activity</h1>
             <form onSubmit={handleSubmit}id="new-activites">
-                <input type='text' name='Title' placeholder='Activity Title'></input>
+                <input onChange={handleChange}type='text' name='Title' placeholder='Activity Title'></input>
                 <input type='submit' value='Submit' />
 
             </form>
