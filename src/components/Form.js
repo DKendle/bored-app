eda8a6a80c70c314b74fd71cef4063ed2a220406
DKendle <dina.kendle@gmail.com>
2021-12-14 15:39:27 -0500

    Moves Form state to parent comp of Activity Display

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,14 @@
 import React from 'react'
-import { useState } from 'react'
-
 
 //State will be used to collect changes in the form and
 //update the original state
-export default function Form() {
-
-    const [title, setTitle] = useState(0)
-
-    function handleSubmit(e){
-        e.preventDefault()
-        console.log(e.target)
-    }
-
-    function handleChange(e){
-        console.log(e.target.value)
-    }
+export default function Form({submit, change, title}) {
 
     return (
         <div>
             <h1>Add a New Activity</h1>
-            <form onSubmit={handleSubmit}id="new-activites">
-                <input onChange={handleChange}type='text' name='Title' placeholder='Activity Title'></input>
+            <form onSubmit={submit}id="new-activites">
+                <input onChange={change}type='text' name='Title' value={title}></input>
                 <input type='submit' value='Submit' />
 
             </form>
