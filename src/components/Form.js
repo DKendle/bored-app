59dd539acba7a624e29dbe74b216546c3cac05db
DKendle <dina.kendle@gmail.com>
2021-12-13 22:16:57 -0500

    Takes form submit listener and creates separate function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 export default function Form() {
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(e.target)
+    }
+
+
     return (
         <div>
             <h1>Add a New Activity</h1>
-            <form id="new-activites">
-                <input type='text' name='Title' placeholder='Title'></input>
-                <input type='submit' name='Submit' />
+            <form onSubmit={handleSubmit}id="new-activites">
+                <input type='text' name='Title' placeholder='Activity Title'></input>
+                <input type='submit' value='Submit' />
 
             </form>
         </div>
