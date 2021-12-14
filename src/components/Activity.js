13b57961ec162b31d830306301f71ce2a7b40059
DKendle <dina.kendle@gmail.com>
2021-12-14 14:34:58 -0500

    adds moree styling

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -5,13 +5,13 @@ export default function Activity({activities}) {
     console.log(activities)
 
     const list = activities.map((a) => {
-        return <li key={a.id}>{a.title}</li>
+        return <li key={a.id} className='list'>{a.title}</li>
     })
     return (
-        <>
+        <div className="act-list">
             <ul>
                 {list}
             </ul>
-        </>
+        </div>
     )
 }
