a40c7edbb10a29302e10d164553b7508ad306760
DKendle <dina.kendle@gmail.com>
2021-12-15 13:17:32 -0500

    Reverts last change. Adds formData object from form info on submit

--- src/components/ActivityDisplay.js
+++ src/components/ActivityDisplay.js
@@ -1,28 +1,12 @@
 import React from 'react'
 import activityArray from '../data/activities'
 import Activity from './Activity'
-import Form from './Form'
-import { useState } from 'react'
 
 export default function ActivityDisplay() {
-
-    const [title, setTitle] = useState('')
-
-    function handleSubmit(e){
-        e.preventDefault()
-        console.log("SUBMITTED")
-    }
-
-    function handleChange(e){
-        setTitle(e.target.value)
-        console.log(title)
-    }
-
     return (
         <div className="act-dis">
             <h1>Activities</h1>
             <Activity activities={activityArray}/>
-            <Form submit={handleSubmit} change={handleChange} title={title}/>
         </div>
     )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 //State will be used to collect changes in the form and
 //update the original state
-export default function Form({submit, change, title}) {
+export default function Form() {
+
+    const [title, setTitle] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const formData = {
+            title: title
+        }
+        console.log(formData)
+        //function to post formData
+    }
+
+    function handleChange(e){
+        setTitle(e.target.value)
+        console.log(title)
+    }
 
     return (
         <div>
             <h1>Add a New Activity</h1>
-            <form onSubmit={submit}id="new-activites">
-                <input onChange={change}type='text' name='Title' value={title}></input>
+            <form onSubmit={handleSubmit}id="new-activites">
+                <input onChange={handleChange}type='text' name='Title' value={title}></input>
                 <input type='submit' value='Submit' />
 
             </form>
